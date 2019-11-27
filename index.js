6ee63e12d171acb468204690915b2e6010437f25
María Beltrán <mariabeltra.98@gmail.com>
2019-11-27 00:31:36 -0300

    install dependencies

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const fs = require('fs')
+const path = require('path')
+const mdLinks = require("...");
+const marked = require('marked');
+const fetch = require('node-fetch');
+
 module.exports = () => {
   // ...
 };
