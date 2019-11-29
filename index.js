da4c9e550d1317f4509184bb5121f2a1f3611999
María Beltrán <mariabeltra.98@gmail.com>
2019-11-29 10:29:22 -0300

    reader md ok

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const fs = require('fs')
 const path = require('path')
-const mdLinks = require("...");
-const marked = require('marked');
-const fetch = require('node-fetch');
+const fs = require('fs')
+//const mdLinks = require("...");
+//const marked = require('marked');
+//const fetch = require('node-fetch');
 
+console.log("¡FUNCIONAAA!")
+
+const readerMd = (path) =>{
+  return new Promise((resolve, reject) => {
+    fs.readFile(path , 'utf-8', (err, data) => {
+      if (err) {
+       console.log('no existe')
+      }
+      resolve(data)
+      console.log(data)
+    })
+  });
+}
+console.log("Please!!!")
+readerMd('./README.md');
 module.exports = () => {
-  // ...
+ 
 };
---
+++ prueba.md
@@ -0,0 +1,3 @@
+# Holi
+# Hola
+# Holo
